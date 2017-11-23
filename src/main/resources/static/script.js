5dc273934c1ab4b0d69f3209cb1700a99ea96383
ioluwayo <ioluwayo@ryerson.ca>
2017-11-23 10:29:01 +0000

    edidata

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -11,7 +11,7 @@ function submitFunction() {
     if(sequence === ''){
         alert("You have to enter a sequence.");
     }
-    $.getJSON('http://localhost:8080/api/translate?sequence='+sequence, {format:"json"}, function(data){
+    $.getJSON('https://immense-citadel-59192.herokuapp.com/'+sequence, {format:"json"}, function(data){
         $(".results").empty();
         for(var i = 0; i<6; i++){
             var update ='<div class="col-md-4 frame">'
